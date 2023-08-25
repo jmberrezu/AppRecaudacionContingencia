b56931cbf5cfb5b859c181a3ddadf2227236c6e8
jmberrezu <67210595+jmberrezu@users.noreply.github.com>
2023-08-25 12:43:13 -0500

    reordenado carpetas

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SupervisorView from "./components/SupervisorView";
-import Login from "./components/Login";
+import SupervisorView from "./components/SupervisorPage/SupervisorView";
+import Login from "./components/RecaudationPage/Login";
 import Recaudation from "./components/RecaudationPage/Recaudation";
-import AdminLogin from "./components/AdminLogin";
-import SupevisorCrud from "./components/SupervisorCrud";
-import SupervisorLogin from "./components/SupervisorLogin";
+import AdminLogin from "./components/AdminPage/AdminLogin";
+import SupevisorCrud from "./components/AdminPage/SupervisorCrud";
+import SupervisorLogin from "./components/SupervisorPage/SupervisorLogin";
 
 function App() {
   return (
